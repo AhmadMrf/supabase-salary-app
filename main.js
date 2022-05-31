ade55084c04d7a5f9b31e2ec8fada74f4be79d18
ahmad-mrf <naashna20@gmail.com>
2022-05-31 11:27:10 +0430

    add get data function

--- main.js
+++ main.js
@@ -3,15 +3,13 @@
 //  import AddPatient from './modules.js'
 // import { LoginSection, PanelAccount, AddPatient } from './modules.js'
 
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
-const supabaseUrl = 'https://evcestdvcdqmklxwirmp.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV2Y2VzdGR2Y2RxbWtseHdpcm1wIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTMzMDk2ODUsImV4cCI6MTk2ODg4NTY4NX0.TsUgJIRuIavxbvz0Ez-sdJ9uuaQHcIGuWrD5lUjHnrw'
-const database = createClient(supabaseUrl, supabaseKey)
-
-
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+const supabaseUrl = "https://evcestdvcdqmklxwirmp.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV2Y2VzdGR2Y2RxbWtseHdpcm1wIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTMzMDk2ODUsImV4cCI6MTk2ODg4NTY4NX0.TsUgJIRuIavxbvz0Ez-sdJ9uuaQHcIGuWrD5lUjHnrw";
+const database = createClient(supabaseUrl, supabaseKey);
 
 // import database from './modules.js'
-
 
 // window.customElements.define('login-section', LoginSection)
 // window.customElements.define('panel-account', PanelAccount)
@@ -21,96 +19,100 @@ const database = createClient(supabaseUrl, supabaseKey)
 // const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV2Y2VzdGR2Y2RxbWtseHdpcm1wIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTMzMDk2ODUsImV4cCI6MTk2ODg4NTY4NX0.TsUgJIRuIavxbvz0Ez-sdJ9uuaQHcIGuWrD5lUjHnrw'
 // const database = createClient(supabaseUrl, supabaseKey)
 
+const loginLabel = document.querySelector("#login-label");
+const loginContent = document.querySelector("#login-content");
+const addPatient = document.querySelector("#add-patient-form");
+const tbody = document.querySelector("table > tbody");
 
-const loginLabel = document.querySelector('#login-label');
-const loginContent = document.querySelector('#login-content');
-const addPatient = document.querySelector('#add-patient-form');
-const tbody = document.querySelector('table > tbody')
-
-let loginForm 
-let emailValue
-let passwordValue
-let fullNameValue
-let jobValue
-let loginBtn 
-
-let isLogin = await database.auth.user()   // data or null
-let activeSession = await database.auth.session()  //data or null
+let loginForm;
+let login_logout_btn;
+let emailValue;
+let passwordValue;
+let fullNameValue;
+let jobValue;
+let loginBtn;
+// let isLogin = true;
+let isLogin = await database.auth.user(); // data or null
+// let activeSession = await database.auth.session()  //data or null
 // let { data: patientData, error } = await database.from('patients')  //data or error
 
-setUpApp(false)
-
+console.log('islogin',isLogin);
+setUpApp(isLogin);
 // loginForm = loginContent.querySelector('form')
-console.log(loginForm.elements);
-
-
-
+console.log(login_logout_btn);
 
 // if sign in retun user info
-// if error return  error obj (error: Object { message: "Invalid login credentials", status: 400 }) 
-async function signInAccount(){
+// if error return  error obj (error: Object { message: "Invalid login credentials", status: 400 })
+async function signInAccount() {
+  console.log('signin');
+  emailValue = loginForm.elements.email;
+  passwordValue = loginForm.elements.password;
 
-emailValue = loginForm.elements.email
-passwordValue = loginForm.elements.password
-
-  const {user , error} = await database.auth.signIn(
-    {
-      email: emailValue.value,
-      password: passwordValue.value,
-    }
-    )
-    if(error){
-      errorManage(error)
-    }
-    return user
+  const { user, error } = await database.auth.signIn({
+    email: emailValue.value,
+    password: passwordValue.value,
+  });
+  // console.log(typeof error);
+  if (error) {
+    errorManage(error);
   }
-  
-  // signInAccount()
+  return user;
+}
 
+// signInAccount()
 
-
-  //if signup return user info
-  //if error return error  (error: Object { message: "User already registered", status: 400 })
-async function signUpAccount(){
-
-emailValue = loginForm.elements.email
-passwordValue = loginForm.elements.password
-fullNameValue = loginForm.elements.fullName
-jobValue = loginForm.elements.job
+//if signup return user info
+//if error return error  (error: Object { message: "User already registered", status: 400 })
+async function signUpAccount() {
+  console.log('signup');
+  emailValue = loginForm.elements.email;
+  passwordValue = loginForm.elements.password;
+  fullNameValue = loginForm.elements.fullName;
+  jobValue = loginForm.elements.job;
 
   const { user, session, error } = await database.auth.signUp(
-      {
-        email: 'me1@email.com',
-        password: '123456',
-      },{
-        data:{
-          fullName:'علی باقری',
-          job:'پرستار'
-        }
-      }
-    )
+    {
+      email: "me1@email.com",
+      password: "123456",
+    },
+    {
+      data: {
+        fullName: "علی باقری",
+        job: "پرستار",
+      },
+    }
+  );
 }
 
-
-async function signOutAccount(){
-//  await database.auth.signOut()
+async function signOutAccount() {
+  console.log('signout');
+  let activeSession = await database.auth.session();
+  return await database.auth.signOut(activeSession.access_token);
 }
 
+async function setUpApp(loginStat,patients=[]) {
+  console.log("setup");
 
-function setUpApp(loginStat) {
-  loginLabel.textContent = loginStat ? `درمانگر : ${'علی باقری'}` : 'ورود - ثبت نام';
+  // let {data:patientsData, error} = loginStat ? await database.from('patients') : [] ;
+  // let patientsData = loginStat ? await getPatientsData(loginStat.id) : [] ;
+  console.log(patients);
 
-  loginContent.innerHTML = loginStat ?
-  `
+  loginLabel.textContent = loginStat
+    ? `درمانگر : ${loginStat.user_metadata.fullName}`
+    : "ورود - ثبت نام";
+
+  loginContent.innerHTML = loginStat
+    ? `
       <div class="panel-account">
         <div class="panel-info">
-          <span>علی باقری</span>
-          <span>پرستار</span>
-          <span>تعداد بیماران : <span id="patient-number" >3</span></span>
+          <span>${loginStat.user_metadata.fullName}</span>
+          <span>${loginStat.user_metadata.job}</span>
+          <span>تعداد بیماران : <span id="patient-number" >${patientsData.length}</span></span>
         </div>
         <button type="button" href="#">خروج</button>
       </div>
-  ` : `
+  `
+    : `
       <form action="#">
         <div class="info">
           <input type="email" name="email" placeholder="ایمیل" />
@@ -118,15 +120,16 @@ function setUpApp(loginStat) {
           <input class="nurse-data" type="text" name="fullName" placeholder="نام و نام خانوادگی" />
           <input class="nurse-data" type="text" name="job" placeholder="سمت و شغل" />
         </div>
-        <button type="button" name="submit" href="#">ورود یا ثبت نام</button>
+        <button type="button" href="#">ورود یا ثبت نام</button>
         <a href="#">فراموشی رمز عبور </a>
       </form>
-  ` ;
+  `;
 
-  loginForm = loginContent.querySelector('form')
+  loginForm = loginContent.querySelector("form");
+  login_logout_btn = loginContent.querySelector("button");
 
-  addPatient.innerHTML = loginStat ? 
-  `
+  addPatient.innerHTML = loginStat
+    ? `
       <form action="#">
         <div class="info">
           <input class="fname" type="text" name="name" placeholder="نام بیمار"/>
@@ -136,11 +139,11 @@ function setUpApp(loginStat) {
         </div>
         <button href="#">افزودن بیمار جدید</button>
       </form>
-  ` : 
-  '<span>ابتدا وارد حساب کاربری خود شوید</span>' ;
+  `
+    : "<span>ابتدا وارد حساب کاربری خود شوید</span>";
 
-  tbody.innerHTML = loginStat ?
-`
+  tbody.innerHTML = loginStat
+    ? `
       <tr>
         <td class="column1">1</td>
         <td class="column2">احمد معروفی</td>
@@ -150,39 +153,56 @@ function setUpApp(loginStat) {
         <td class="column6"><button>edit</button></td>
       </tr>
 `
-  :
-'<tr><td class="fake-td" colspan="6">برای مشاهده اطلاعات وارد حساب کاربری خود شوید</td></tr>'
-  ;
+    : '<tr><td class="fake-td" colspan="6">برای مشاهده اطلاعات وارد حساب کاربری خود شوید</td></tr>';
+}
 
+function errorManage(errorMassage) {
+  console.log('error manage');
+  console.log(errorMassage, "مدیریت خطا");
+}
+
+
+async function getPatientsData(id){
+console.log("get");
+ let {data, error} = await database.from('patients')
+ return data ? data:error ;
 
 }
 
-function errorManage(){
-  console.log(error, 'مدیریت خطا');
-}
 
-loginForm.elements.submit.addEventListener('click', ()=> {
-let userResponse = signInAccount()
+// console.log(login_logout_btn);
 
-userResponse.then(user=>{
-  if(user){
-    // setUpApp(user)
-  }else{
-    console.log(user, ' خطا');
+login_logout_btn.addEventListener("click", async () => {
+
+  if (isLogin) {
+    // sign out btn active
+    let { error } = await signOutAccount();
+    if (error) {
+      console.log("has error", error);
+    } else {
+      console.log("no error", error);
+      setUpApp(error);
+    }
+  } else {
+    console.log('click');
+    //sign in btn active 
+    let userData = await signInAccount();
+    if (userData) {
+      setUpApp(userData);
+    } else {
+      console.log(userData, " خطا");
+    }
   }
-  })
-})
+});
 
 // signOutBtn?signOutBtn.addEventListener('click', async ()=>{
-  // await database.auth.signOut(activeSession.access_token)
+// await database.auth.signOut(activeSession.access_token)
 //   setUpApp(await database.auth.user())
 //   console.log('loge out succesfully')
 // }) : console.log('no'); ;
 
 // async function logOut(){
 // }
-
-
 
 //  await database.auth.signOut()
 // const session = await database.auth.session()
@@ -191,12 +211,8 @@ userResponse.then(user=>{
 // console.log(session);
 // console.log(c);
 
-
-
-
-
-
-//   let {data} = await database.from('patients')
+  // let g = await database.from('4patients')
+  // console.log(g);
 // let tr
 // let trs = data.map( (el, i) => {
 //   return tr = `<tr>
@@ -227,7 +243,6 @@ userResponse.then(user=>{
 //   password: "0123456",
 //   data: { fullName:'علی باقری',job:' کارشناس زخم' }
 // })
-
 
 // let c = await database.auth.signOut()
 
